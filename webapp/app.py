3be444ef361eaffad83479919c24f45c54c92561
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-09-23 21:30:31 +0400

    Added PWA functionality

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
+# First two routes set-up PWA configuration
+# Intitaties Manifest Route
+@app.route("/manifest.json")
+def manifest():
+    return send_from_directory("/home/azfar/Workout-Log/webapp/", "manifest.json")
+
+# Intiates service-worker route
+@app.route("/service-worker.js")
+def service_worker():
+    return send_from_directory("/home/azfar/Workout-Log/webapp/", "service-worker.js")
+
+# Intiates CSS and JS for the HTML page
+
+
+# Defines index route
+@app.route("/")
 def hello():
-    return 'Hello, World'
+    return render_template("layout.html")
