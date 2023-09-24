73c576b7e9e568f9b738e909d05d5da410bd2c99
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-09-24 21:13:21 +0400

    added basic routes

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, session, redirect, request
+from flask_session import Session
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
 
 app = Flask(__name__)
 
-# First two routes set-up PWA configuration
+# Connects to users table
+connUsers = sqlite3.connect("users.db")
+usersCursor = connUsers.cursor()
+
+# Configures Flask to use Server-Side Session Storage
+app.config["SESSION_TYPE"] ="filesystem"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_USE_SIGNER"] = True
+
+#####################################################################
+# Sets PWA Config
 # Intitaties Manifest Route
 @app.route("/manifest.json")
 def manifest():
@@ -17,12 +30,46 @@ def service_worker():
 @app.route("/templates/static/styles.css")
 def css():
     return send_from_directory("/home/azfar/Workout-Log/webapp/templates/static", "styles.css")
-
+# Intiates app.js route
 @app.route("/templates/static/app.js")
 def app_js():
     return send_from_directory("/home/azfar/Workout-Log/webapp/templates/static", "app.js")
 
+#####################################################################
+
 # Defines index route
 @app.route("/")
-def hello():
-    return render_template("layout.html")
+def index():
+    # Checks if user is Logged in
+    if "username" in session:
+        return render_template("index.html")
+    else:
+        return redirect("/login")
+    
+    return redirect("/error")
+
+# Defines Login Route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Receives username and password
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # If Login successful
+        session["username"] = username
+        return redirect("/")
+
+# Default case returns login page
+    return render_template("login.html")
+
+#Logsout
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect("/login")
+
+#Error Route
+@app.route("/error")
+def error():
+    return "Error"
