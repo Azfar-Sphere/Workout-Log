0f36e23a82fff8065d55d1b6c59f02b90669e043
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-10-01 17:25:56 +0400

    Made auth.py removed session functonality

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask, render_template, send_from_directory, session, redirect, request, url_for, flash
 from flask_session import Session
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 from pwa_routes import pwa_bp
 from tables import User
@@ -10,18 +9,15 @@ from tables import User
 app = Flask(__name__)
 app.secret_key = "$2y$10$MQ72/iHjmp16XETNlq1E..BMlHrAGmMkHOxhu8MfO7.7toUb6fXdq"
 
+if __name__ == "__app__":
+    app.run(debug=True)
+
 # Registers Blueprints
 app.register_blueprint(pwa_bp)
 
 # Connects to users table
 db = SQLAlchemy()
 DB_NAME = "users.db"
-
-# Configures Flask to use Server-Side Session Storage
-app.config["SESSION_TYPE"] = "filesystem"
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_USE_SIGNER"] = True
-Session(app)
 
 # Configures Database
 db_path = os.path.join(app.root_path, DB_NAME)
@@ -54,71 +50,6 @@ def index():
     else:
         return render_template("index.html")
         
-    
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        # Gets Password
-        username = request.form.get("username")
-        password = request.form.get("password")
-        conpassword = request.form.get("conpassword")
-
-        # Gets username for database
-        user = User.query.filter_by(username=username).first()
-        # Checks valid username and password
-        if not username or not password:
-            flash("Please enter username and password", category='error')
-
-        elif username in user:
-            flash("Username taken", category='error')
-
-        elif password != conpassword:
-            flash("Passwords dont match", category='error')
-
-        else:
-            # Adds user detail to users database
-            passwordHash = generate_password_hash("password")
-            new_user = User(username=username, password=passwordHash)
-            db.session.add(new_user)
-            db.session.commit()
-
-            login_user(user)
-            flash("Successfully registered", category="success")
-            return redirect(url_for("index"))
-    
-    return render_template("register.html")
-
-# Defines Login Route
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    # Receives username and password
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        user = User.query.filter_by(username=username).first()
-        if user is None:
-            flash("Incorrect username", category='error')
-
-        elif not check_password_hash(user.password, password):
-            flash("Incorrect Password", category='error')
-
-        # If Login successful
-        login_user(user, remember=True)
-        flash("Logged in successfully", category='success')
-        return redirect(url_for("index"))
-
-    return render_template("login.html")
-
-#Logsout
-@app.route("/logout")
-@login_required
-def logout():
-    session.pop("username", None)
-    logout_user()
-    return redirect(url_for("login"))
-
 @app.route("/error")
 def error():
     return render_template("error.html")    
---
+++ webapp/auth.py
@@ -0,0 +1,69 @@
+# Handles Authentication functionality
+from app import db  
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from tables import User
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # Gets Password
+        username = request.form.get("username")
+        password = request.form.get("password")
+        conpassword = request.form.get("conpassword")
+
+        # Gets username for database
+        user = User.query.filter_by(username=username).first()
+        # Checks valid username and password
+        if not username or not password:
+            flash("Please enter username and password", category='error')
+
+        elif username in user:
+            flash("Username taken", category='error')
+
+        elif password != conpassword:
+            flash("Passwords dont match", category='error')
+
+        else:
+            # Adds user detail to users database
+            passwordHash = generate_password_hash("password")
+            new_user = User(username=username, password=passwordHash)
+            db.session.add(new_user)
+            db.session.commit()
+
+            login_user(user)
+            flash("Successfully registered", category="success")
+            return redirect(url_for("index"))
+    
+    return render_template("register.html")
+
+# Defines Login Route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Receives username and password
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash("Incorrect username", category='error')
+
+        elif not check_password_hash(user.password, password):
+            flash("Incorrect Password", category='error')
+
+        # If Login successful
+        login_user(user, remember=True)
+        flash("Logged in successfully", category='success')
+        return redirect(url_for("index"))
+
+    return render_template("login.html")
+
+#Logsout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
