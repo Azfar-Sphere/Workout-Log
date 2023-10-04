b8579fe4b12529134a07e1bf9fe7177f611ba0a5
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-10-04 10:05:55 +0400

    Added ability to add new workouts

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,13 +1,22 @@
-from flask import render_template, Blueprint
-from flask_login import login_required
+from flask import render_template, Blueprint, request
+from flask_login import login_required, current_user
+from .tables import Workout
+from . import db
 
 routes = Blueprint("routes", __name__)
 
 # Defines index route
-@routes.route("/")
+@routes.route("/", methods=["GET", "POST"])
 @login_required
 def index():
-    return render_template("index.html")
+    # Adds new workout
+    if request.method == "POST":
+        new_workout = Workout(user_id = current_user.id)
+        db.session.add(new_workout)
+        db.session.commit()
+
+    workouts = db.session.query(Workout).all()
+    return render_template("index.html", workouts=workouts)
 
 @routes.route("/error")
 def error():
--- webapp/tables.py
+++ webapp/tables.py
@@ -2,6 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+# Creats user and workout tables
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
