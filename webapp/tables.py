c83312422847adc5eceaf9c4927a7128dc801aa7
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-10-04 10:33:49 +0400

    tried to fix work column property

--- webapp/tables.py
+++ webapp/tables.py
@@ -1,6 +1,8 @@
 from . import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
+from sqlalchemy.sql import func, select
+from sqlalchemy.orm import column_property
+from flask_login import current_user
 
 # Creats user and workout tables
 class User(db.Model, UserMixin):
@@ -10,6 +12,14 @@ class User(db.Model, UserMixin):
 
 class Workout(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    work =  db.Column(db.Integer, nullable=False, default=lambda: Workout.query.count() + 1)
     date = db.Column(db.DateTime(timezone=True), default = func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    work =  column_property(
+    select([func.coalesce(func.max(Workout.work), 0) + 1])
+    .where(Workout.user_id == current_user.id)
+    .correlate_except(Workout)
+    .scalar_subquery()
+)
+# Makes it so that each user has their own work value that increments by 1 with each workout
+
+
