7f7793597e3ed4a145922f11fbd11e7ac9dafe94
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-10-04 09:48:22 +0400

    Added Workout table

--- webapp/tables.py
+++ webapp/tables.py
@@ -1,8 +1,14 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    work =  db.Column(db.Integer, nullable=False, default=lambda: Workout.query.count() + 1)
+    date = db.Column(db.DateTime(timezone=True), default = func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
