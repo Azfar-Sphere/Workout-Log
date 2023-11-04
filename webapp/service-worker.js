0e43fa909ba6cac28f7b4a484e90d4739f60cdba
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-11-04 18:36:58 +0400

    Added Workbox to service-worker

--- webapp/service-worker.js
+++ webapp/service-worker.js
@@ -1,1 +1,6 @@
-importScripts('cdn://workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.1.0/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.NetworkFirst()
+)
