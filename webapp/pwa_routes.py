eddaa713f692e840f4ee4dc1b9857cc39274327d
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-11-11 23:53:14 +0400

    Fixed static path, removed sw and dependencies

--- webapp/pwa_routes.py
+++ webapp/pwa_routes.py
@@ -3,25 +3,24 @@ import os
 
 pwa_bp = Blueprint("pwa", __name__)
 
-# Sets routes for all
-@pwa_bp.route('/<path:filename>')
+# Sets routes for all possible paths
+@pwa_bp.route("/<filename>")
 def root_path(filename):
     directory = os.path.join(os.getcwd())
     return send_from_directory(directory, filename)
 
-@pwa_bp.route('/webapp/<path:filename>')
+@pwa_bp.route("/webapp/<filename>")
 def webapp_path(filename):
     directory = os.path.join(os.getcwd(), "webapp")
     return send_from_directory(directory, filename)
 
-@pwa_bp.route('/webapp/templates/<path:filename>')
+@pwa_bp.route("/webapp/templates/<filename>")
 def templates_path(filename):
     directory = os.path.join(os.getcwd(), "webapp/templates")
     return send_from_directory(directory, filename)
 
-@pwa_bp.route('/webapp/templates/static/<path:filename>')
+@pwa_bp.route("/webapp/templates/static/<filename>")
 def static_path(filename):
     directory = os.path.join(os.getcwd(), "webapp/templates/static")
-    print("Directory:", directory)
-    print("Filename:", filename)
     return send_from_directory(directory, filename)
+
