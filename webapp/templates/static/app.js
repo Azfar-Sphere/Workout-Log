1cf8b502d1592918599eaf3e609ada36c6764dc7
Azfar <s.azfar.rahman@gmail.com>
2023-12-08 17:19:18 +0400

    Moved Scripts to app.js

--- webapp/templates/static/app.js
+++ webapp/templates/static/app.js
@@ -9,3 +9,37 @@ function confirmLogout() {
         return false;
     }
 };
+
+function confirmDeleteSets() {
+    var confirmation = confirm("Are you sure you want to delete your sets?");
+
+    if (confirmation) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+function confirmWeek() {
+    var confirmation = confirm("Are you sure you want to increment the week? This action cannot be reversed!");
+
+    if (confirmation) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+function confirmDeleteExercise(day, exercise) {
+    var confirmation = confirm("Are you sure you want to delete this exercise?");
+
+    if (confirmation) {
+        window.location.href = "/delete_e/" + day + "/" + exercise;
+        return true;
+    }
+    else {
+        return false;
+    }
+};
